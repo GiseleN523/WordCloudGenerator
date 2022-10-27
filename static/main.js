bdd498e4fa9ac13dcdaca7ba71772a69c1b107a3
GiseleN523 <giselenelson@rocketmail.com>
2022-10-27 13:20:07 -0500

    interface appearance improvements

--- static/main.js
+++ static/main.js
@@ -4,17 +4,22 @@ define(['d3', 'app', 'https://sharonchoong.github.io/svg-exportJS/svg-export.min
 
   let fileUploadLast = false; //keeps track of whether a file has been uploaded or the textarea input changed more recently, to know which one to use when generating
 
+  //flex for right column is just set to "auto" with css, so adjust it here to actually fill the rest of the space
+  document.getElementById("rightContentColumn").style.flex = (window.innerWidth-(.93*window.innerHeight)-50)+"px";
+  //this should have been set in the css, but make sure here that it is the same value as the value passed to app and then to d3-cloud
+  document.getElementById("wordCloudPreview").style.height = .93*window.innerHeight;
+
   let colorSchemes = [d3.schemeTableau10, d3.schemeSet1, d3.schemeDark2, d3.schemeSet2, d3.schemeCategory10, ["#e60049", "#0bb4ff", "#50e991", "#e6d800", "#9b19f5", "#ffa300", "#dc0ab4", "#b3d4ff", "#00bfa0"], ["#b30000", "#7c1158", "#4421af", "#1a53ff", "#0d88e6", "#00b7c7", "#5ad45a", "#8be04e", "#ebdc78"]];
   let colorSchemesText = ["Color Scheme 1", "Color Scheme 2", "Color Scheme 3", "Color Scheme 4", "Color Scheme 5", "Color Scheme 6", "Color Scheme 7"];
   colorSchemesText.forEach(d => document.getElementById("groupColorPref").innerHTML+='<option value="'+d+'">'+d+'</option>');
   document.querySelectorAll("#customColors input").forEach((d, i) => d.value = colorSchemes[0][i]);
 
   document.getElementById("stopWordsBoxPref").value = app.stopWords.toString().replaceAll(",", " "); //populate stop words textarea with app's default stopwords
-
+  
   document.getElementById('generateButton').onclick = function()
   {
     document.getElementById("showAllWords").checked = false;
-    app.dimPref = 700; //if changed, must also be changed in styles.css; TODO: connect these two
+    app.dimPref = .93*window.innerHeight;
     app.paddingPref = document.getElementById('paddingPref').value;
     app.numWordsPref = document.getElementById('numWordsPref').value;
     app.minCountPref = document.getElementById('minCountPref').value;
@@ -69,15 +74,39 @@ define(['d3', 'app', 'https://sharonchoong.github.io/svg-exportJS/svg-export.min
 
   document.getElementById("groupColorPref").onchange = () => document.querySelectorAll("#customColors input").forEach((d, i) => d.value = colorSchemes[colorSchemesText.indexOf(document.getElementById("groupColorPref").value)][i]);
   
-  document.getElementById("fileInput").onchange = (e) => e.target.files.length>0 ? fileUploadLast = true : fileUploadLast = false;
-
-  document.getElementById("rawTextInput").onchange = (e) => e.target.value.length>0 ? fileUploadLast = false : fileUploadLast = true;
+  document.querySelectorAll("#customColors input").forEach(d => d.onchange = function()
+  {
+    document.querySelectorAll("#customColors input").forEach((d, i) => d.value = colorSchemes[0][i]);
+  })
 
   document.getElementById("stopWordsPref").onchange = (e) => e.target.checked ? document.getElementById("stopWordsBoxPrefDiv").style.display = "block" : document.getElementById("stopWordsBoxPrefDiv").style.display = "none";
 
   document.getElementById("stopWordsBoxPref").onchange = () => app.stopWords = document.getElementById("stopWordsBoxPref").value.split(" ");
 
-   //load more words to list of "extra words" if user approaches the bottom of currently loaded list
+  function hideGroup(group) //hide/compress settings group and switch h3 (tab header) event listener to show/expand group when clicked
+  {
+    group.querySelectorAll(".prefGroupContent")[0].style.display = "none";
+    group.querySelectorAll("h3")[0].onclick = (e) => showGroup(e.target.parentNode);
+  }
+
+  function showGroup(group) //show/expand settings group and switch h3 (tab header) event listener to hide/compress group when clicked
+  {
+    group.querySelectorAll(".prefGroupContent")[0].style.display = "block";
+    group.querySelectorAll("h3")[0].onclick = (e) => hideGroup(e.target.parentNode);
+  }
+
+  //hide groups if their tab header is clicked
+  document.querySelectorAll(".prefGroup h3").forEach((d) => d.onclick = (e) => hideGroup(e.target.parentNode));
+
+  //close top text input group automatically if user clicks away from it
+  document.querySelectorAll("#fileInput, #rawTextInput").onFocusOut = (e) => e.target.parentNode.style.display = "none";
+
+  //update fileUploadLast variable with whether a file was just uploaded, in order to know where to get the text from
+  document.getElementById("fileInput").onchange = (e) => e.target.value.length>0 ? fileUploadLast = true : fileUploadLast = false;
+  document.getElementById("rawTextInput").onchange = (e) => e.target.value.length>0 ? fileUploadLast = false : fileUploadLast = true;
+
+
+  //load more words to list of "extra words" if user approaches the bottom of currently loaded list
   document.getElementById("extraWordsList").onscroll = function() {
     let extraWordsElem = document.getElementById("extraWordsList");
     if(extraWordsElem.scrollTop + extraWordsElem.clientHeight + 20 >= extraWordsElem.scrollHeight) 
@@ -112,7 +141,7 @@ define(['d3', 'app', 'https://sharonchoong.github.io/svg-exportJS/svg-export.min
   {    
     document.getElementById("wordCloudPreview").append(svg);
 
-    document.getElementById("downloadSvgButton").style.display = "block";
+    document.getElementById("downloadSvgButton").style.display = "inline-block";
     document.getElementById("downloadSvgButton").onclick = () => svgExportJS.downloadSvg(app.svg.node(), "yourwordcloud");
 
     document.getElementById("wordCount").innerHTML = " | Number of Unique Words (excluding stop words): "+(app.words.length+app.extraWords.length);
